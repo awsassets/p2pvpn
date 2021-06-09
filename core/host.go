8780f48061cd0d71aa45865320677ddaf9fd0a73
xjasonlyu <xjasonlyu@gmail.com>
2021-06-09 14:59:48 +0800

    chore: core cleanup

--- core/host.go
+++ core/host.go
@@ -9,12 +9,19 @@ import (
 	"github.com/libp2p/go-libp2p-core/host"
 )
 
-// NewHost creates a libp2p host with a generated identity.
-func NewHost(address string, port int) host.Host {
+var _host host.Host = nil
+
+func Host() host.Host {
+	return _host
+}
+
+// InitHost creates a libp2p host with a generated identity.
+func InitHost(address string, port int) host.Host {
 	h, err := libp2p.New(context.Background(),
 		libp2p.ListenAddrStrings(fmt.Sprintf("/ip4/%s/tcp/%d", address, port)))
 	if err != nil {
 		log.Fatalln(err)
 	}
+	_host = h
 	return h
 }
