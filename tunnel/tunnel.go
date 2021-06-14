61a817c80b8572757a54e102ae274c20d3478a4c
xjasonlyu <xjasonlyu@gmail.com>
2021-06-14 16:28:19 +0800

    feat: export relay

--- tunnel/tunnel.go
+++ tunnel/tunnel.go
@@ -17,6 +17,9 @@ func Add(ctx context.ConnContext) {
 	tcpQueue <- ctx
 }
 
+// Relay exports internal relay function.
+var Relay = relay
+
 func process() {
 	for c := range tcpQueue {
 		go handleTCPConn(c)
